8924761521ae04f3a18d06efa2de8e20b9cd6d30
Happystar2018 <happystar2018@naver.com>
2021-02-02 04:36:00 +0000

    Footer.js: updated div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="copyright">
-      &copy; RoadmapInc
+      &copy; CHR of RoadmapInc
       <a href="https://html5up.net">HTML5 UP</a>.
     </div>
   );
