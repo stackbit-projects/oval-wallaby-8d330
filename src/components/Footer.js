7adce7fd5cd91727c8bfc04f823e06b9b2490450
Happystar2018 <happystar2018@naver.com>
2021-02-02 04:35:47 +0000

    Footer.js: updated div

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="copyright">
-      &copy; Paradigm Shift. All rights reserved. Design:
+      &copy; Roadmap
       <a href="https://html5up.net">HTML5 UP</a>.
     </div>
   );
